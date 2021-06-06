245bfb9eb63096a1c27b40549cc8ce809cf95aff
zcassini <zcassini@gmail.com>
2021-06-06 10:58:51 -0400

    init

---
+++ src/main/java/org/wcci/team1/trekproject/TrekProjectApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.team1.trekproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wcci/team1/trekproject/TrekProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wcci.team1.trekproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
