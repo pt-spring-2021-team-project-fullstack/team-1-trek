d62863aae7ac5aef1cc528d781e4589d6fb129b1
zcassini <zcassini@gmail.com>
2021-06-16 18:16:16 -0400

    routing controller and everything uses the layout

---
+++ src/main/java/org/wcci/team1/trekproject/RoutesController.java
@@ -0,0 +1,26 @@
+package org.wcci.team1.trekproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RoutesController {
+
+    @RequestMapping("/")
+    public String home(Model model) {
+//        model.addAttribute("title", "Home");
+        return "home";
+    }
+    @RequestMapping("/aboutUs")
+    public String aboutUs(Model model) {
+        model.addAttribute("title", "About Us");
+        return "aboutUs";
+    }
+
+    @RequestMapping("/contactUs")
+    public String contactUs(Model model) {
+        model.addAttribute("title", "Contact Us");
+        return "contactUs";
+    }
+}
