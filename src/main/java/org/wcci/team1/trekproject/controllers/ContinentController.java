4bb00422f27e08aefb96794c7b0143aee2a3ca13
roseholmesminton <rose.yvonne216@icloud.com>
2021-06-18 19:08:50 -0400

    continents does not work

--- src/main/java/org/wcci/team1/trekproject/controllers/ContinentController.java
+++ src/main/java/org/wcci/team1/trekproject/controllers/ContinentController.java
@@ -2,7 +2,11 @@ package org.wcci.team1.trekproject.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.wcci.team1.trekproject.models.Continent;
+import org.wcci.team1.trekproject.models.Region;
 import org.wcci.team1.trekproject.repositories.ContinentRepository;
 
 import javax.annotation.Resource;
@@ -17,5 +21,17 @@ public class ContinentController {
     public String displayContinents(Model model) {
         model.addAttribute("continents", continentRepo.findAll());
         return "continentsView";
+    }
+
+
+    @GetMapping("/continents/{name}")
+    public String displaySingleContinent(@PathVariable String name, Model model) {
+        Continent retrievedContinent = continentRepo.findContinentByName(name);
+        model.addAttribute("continent", retrievedContinent);
+        retrievedContinent.getName();
+        retrievedContinent.getDescription();
+        retrievedContinent.getImage();
+
+        return "continentView";
     }
 }
--- src/main/java/org/wcci/team1/trekproject/repositories/ContinentRepository.java
+++ src/main/java/org/wcci/team1/trekproject/repositories/ContinentRepository.java
@@ -2,14 +2,12 @@ package org.wcci.team1.trekproject.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.wcci.team1.trekproject.models.Continent;
-import org.wcci.team1.trekproject.models.Region;
-import org.wcci.team1.trekproject.models.Trek;
+
 
 import java.util.Optional;
 
 public interface ContinentRepository extends CrudRepository<Continent, Long> {
-    @Override
-    Optional<Continent> findById(Long aLong);
-    Region findContinentByName(String name);
+    Optional<Continent> findById(long id);
+    Continent findContinentByName(String name);
 }
 
